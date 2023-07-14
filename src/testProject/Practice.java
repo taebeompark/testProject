fcee286b792ceeae74db5b84791ba689f316b4c4
taebeom <ptbyahoo12@gmail.com>
2023-07-14 17:30:55 +0900

    연습2

--- src/testProject/Practice.java
+++ src/testProject/Practice.java
@@ -1,5 +1,12 @@
 package testProject;
-
+import java.util.Scanner;
 public class Practice {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("문자입력: ");
+		String input = sc.nextLine();
+		
+		System.out.printf("%s",input);
+	}
 }
