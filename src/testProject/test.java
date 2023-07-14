bb89aa347774e6396a4d2205282b5d7de2b5de32
taebeom <ptbyahoo12@gmail.com>
2023-07-14 17:03:52 +0900

    test.java commit

---
+++ src/testProject/test.java
@@ -0,0 +1,18 @@
+package testProject;
+import java.util.Scanner;
+public class test {
+	public static void main(String[] args) {
+		Scanner ptb = new Scanner(System.in);
+		
+		System.out.print("문자열 입력:");
+		String input = ptb.nextLine();
+		
+		System.out.println(input.charAt(0)+":"+ (int)input.charAt(0) );
+		System.out.println(input.charAt(1)+":"+ (int)input.charAt(1) );
+		System.out.println(input.charAt(2)+":"+ (int)input.charAt(2) );
+		System.out.println(input.charAt(3)+":"+ (int)input.charAt(3) );
+		System.out.println(input.charAt(4)+":"+ (int)input.charAt(4) );
+		
+	}
+
+}
