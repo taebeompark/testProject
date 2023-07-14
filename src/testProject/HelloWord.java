fcf77029c016835c1a98f8209a17d3139d1aa975
taebeom <ptbyahoo12@gmail.com>
2023-07-14 14:50:40 +0900

    first commit

---
+++ src/testProject/HelloWord.java
@@ -0,0 +1,21 @@
+package testProject;
+
+
+
+public class HelloWord {
+	// : 한줄 주석
+	/*범위주석
+	 * 
+	 * 
+	 *
+	 */
+	
+	
+	//메인 메서드
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("그래그래");
+		System.out.println("나야나");
+	}
+	
+}
